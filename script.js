9dad873cbab579ec90883b5140cdbb8b5d51dc8a
mylifeasggy <ginellysmsuarez@gmail.com>
2025-05-19 14:37:59 -0400

    Requirements added

---
+++ script.js
@@ -0,0 +1,14 @@
+//Cache at least one element using selectElementById.
+//Requirement 2: Cache elements using querySelector
+// Requirement 3: Use parent-child-sibling relationships
+//Requirement 4: Iterate over a collection of elements to accomplish a task
+//Create at least one element using createElement.
+//Use appendChild and/or prepend to add new elements to the DOM.
+//Use the DocumentFragment interface or HTML templating with the cloneNode method to create templated content. 
+//Modify the HTML or text content of at least one element in response to user interaction using innerHTML, innerText, or textContent.
+//Modify the style and/or CSS classes of an element in response to user interactions using the style or classList properties.
+//Modify at least one attribute of an element in response to user interaction.
+//Register at least two different event listeners and create the associated event handler functions.
+//Use at least two Browser Object Model (BOM) properties or methods.
+//Include at least one form and/or input with HTML attribute validation.
+//Include at least one form and/or input with DOM event-based validation. (This can be the same form or input as the one above, but should include event-based validation in addition to the HTML attribute validation.)
